bacdc0d62fdc1e9500bbdd545a8a429220f81065
Bubbelbad <victor.ivarson@hotmail.se>
2024-11-23 14:35:26 +0100

    Added architecture, weatherForecast and mediatR

---
+++ Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
